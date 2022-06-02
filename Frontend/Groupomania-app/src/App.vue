17d3e981a0482acfd0c3b00838ab20d1f5d5475a
Nadia-github <nadia.lahssini@yahoo.fr>
2022-06-02 15:51:32 +0200

    modifs

--- Frontend/Groupomania-app/src/App.vue
+++ Frontend/Groupomania-app/src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
+    <div id="nav">
+      <router-link to="/">connexion</router-link>
+      <router-link to="/inscription">inscription</router-link>
+    </div>
     <header>
       <h1>
-        Groupomania network
+        The Groupomania network
       </h1>
     </header>
     <main>
       <connexion/> 
     </main>
     <footer>
-      fait par Nadia
+      © copyright Groupomania 2022
     </footer>
+    <router-view/>
   </div>
 </template>
 
 
 <script>
 import connexion from "@/components/connexion.vue";
+import RouterLink from "@/router"
 export default {
   name: "App",
   components: {connexion},
--- Frontend/Groupomania-app/src/main.js
+++ Frontend/Groupomania-app/src/main.js
@@ -4,6 +4,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import * as VueRouter from 'vue-router'
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+      {
+      path: '/',
+      name: 'connexion',
+      component: connexion
+      },
+      {
+          path: '/inscription',
+      name: 'inscription',
+      component: inscription
+      }
+    ]
+  })
 
 loadFonts()
 
