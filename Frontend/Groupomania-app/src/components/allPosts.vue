1f12e4cd3ff3d3f6c63e752e05880c9356739c4b
Nadia-github <nadia.lahssini@yahoo.fr>
2022-06-18 02:40:40 +0200

    finalisation

--- Frontend/Groupomania-app/src/components/allPosts.vue
+++ Frontend/Groupomania-app/src/components/allPosts.vue
@@ -1,47 +1,75 @@
 <template>
   <v-app>
-    
-      <h2 class="titre_deconnecte">Bonjour {{ prenom }}, alors quoi de neuf ?</h2>
-      <div class="divos justify-space-around">
-        <a href="/createpost" class="link-a">
-          <v-btn class="btn_createpost">
-            <v-btn margin="0 auto" id="btn_icon" color="grey" icon="mdi-pencil" size="x-small"></v-btn>
-            Créer une publication</v-btn
-          >
-        </a>
-      </div>
-      <v-btn @click="logout" id="btn-logout">Déconnexion </v-btn>
-      <v-main class="main">
-        <v-container fluid>
-          <v-card 
+    <h2 class="titre_deconnecte">Bonjour {{ prenom }}, alors quoi de neuf ?</h2>
+    <div class="divos justify-space-around">
+      <a href="/createpost" class="link-a">
+        <v-btn class="btn_createpost">
+          <v-btn
+            margin="0 auto"
+            id="btn_icon"
+            color="grey"
+            icon="mdi-pencil"
+            size="x-small"
+          ></v-btn>
+          Créer une publication</v-btn
+        >
+      </a>
+    </div>
+    <v-btn @click="logout" id="btn-logout">Déconnexion </v-btn>
+    <v-main class="main">
+      <v-container fluid>
+        <v-card
           v-for="post in posts"
-          :key="post.id"    
-          style="border-radius:20px; 
-          "
+          :key="post.id"
+          style="border-radius: 20px"
           class="mb-2 card-remix"
           density="compact"
           color-elevation="blue"
-          
         >
           <div class="presentation">
-            <img class="img-id" v-if="post.user.sexe == 'H'" src="/homme.png" alt="homme">
-            <img class="img-id" v-if="post.user.sexe == 'F'" src="/femme.png" alt="femme">
-           
-            <h4>{{ post.user.prenom }}  {{ post.user.nom }} </h4>
-          </div>
-          <div id="titre_disposition" style="text-align: center">{{ post.titre }}</div>
-          
-          <img v-if="post.attachment" v-bind:src="post.attachment" id="img-attachment"/>
-        
-        
+            <img
+              class="img-id"
+              v-if="post.user.sexe == 'H'"
+              src="/homme.png"
+              alt="homme"
+            />
+            <img
+              class="img-id"
+              v-if="post.user.sexe == 'F'"
+              src="/femme.png"
+              alt="femme"
+            />
 
-          <v-card-text style="text-align: justify; display: flex; flex-direction: column;">
-            <div>{{ post.contenu }}</div> <br> <br>
-            <div id="dateCreation">Commentaire créé le :  {{ dateTime(post.createdAd) }} </div>
+            <h4>{{ post.user.prenom }} {{ post.user.nom }}</h4>
+          </div>
+          <div id="titre_disposition" style="text-align: center">
+            {{ post.titre }}
+          </div>
+
+          <img
+            v-if="post.attachment"
+            v-bind:src="post.attachment"
+            id="img-attachment"
+          />
+
+          <v-card-text
+            style="text-align: justify; display: flex; flex-direction: column"
+          >
+            <div>{{ post.contenu }}</div>
+            <br />
+            <br />
+            <div id="dateCreation">
+              Commentaire créé le : {{ dateTime(post.createdAt) }}
+            </div>
           </v-card-text>
-          
+
           <template v-slot:actions>
-            <v-btn v-if="post.userId == userId || isAdmin" @click="deletePost(post)" id="deletePost">Supprimer</v-btn>
+            <v-btn
+              v-if="post.userId == userId || isAdmin"
+              @click="deletePost(post)"
+              id="deletePost"
+              >Supprimer</v-btn
+            >
           </template>
         </v-card>
       </v-container>
@@ -49,9 +77,8 @@
   </v-app>
 </template>
 
-
 <script>
-import moment from 'moment';
+import moment from "moment";
 export default {
   data: () => ({
     isAdmin: parseInt(localStorage.getItem("isAdmin")),
@@ -59,13 +86,11 @@ export default {
     prenom: localStorage.getItem("prenom"),
     nom: localStorage.getItem("nom"),
     userId: localStorage.getItem("userId"),
-    posts: [
-
-    ],
+    posts: [],
   }),
-  
-  mounted() {    
-    const token = localStorage.getItem("token")
+
+  mounted() {
+    const token = localStorage.getItem("token");
     fetch("http://localhost:5000/api/posts/", {
       method: "GET",
       headers: {
@@ -73,23 +98,22 @@ export default {
         Authorization: "Bearer " + token,
       },
     })
-    .then(response => response.json())
-    .then((post) => {
-        console.log(post)
-        this.posts = post.posts
-    });
+      .then((response) => response.json())
+      .then((post) => {
+        console.log(post);
+        this.posts = post.posts;
+      });
   },
   methods: {
-    
     deletePost(post) {
-      const token = localStorage.getItem("token")   
-      fetch("http://localhost:5000/api/posts/"+post.id, {
-        method : "DELETE",
+      const token = localStorage.getItem("token");
+      fetch("http://localhost:5000/api/posts/" + post.id, {
+        method: "DELETE",
         headers: {
           "Content-Type": "application/json",
           Authorization: "Bearer " + token,
-        },    
-      })
+        },
+      });
       window.location.reload();
     },
 
@@ -106,104 +130,102 @@ export default {
 </script>
 
 <style>
-
-.v-btn{
+.v-btn {
   cursor: pointer;
 }
 
 .card-remix {
-    box-shadow: 0px 1px 10px 3px rgba(203, 198, 198, 0.95);;
-    background-color: rgb(245, 239, 239);
-    padding: 10px;
+  box-shadow: 0px 1px 10px 3px rgba(203, 198, 198, 0.95);
+  background-color: rgb(245, 239, 239);
+  padding: 10px;
 }
 
 .card-remix:hover {
-    box-shadow: 0px 1px 10px 3px rgba(189, 177, 177, 0.95);
-    background-color: rgb(255, 255, 255);
-    transform: scale(1.02);
+  box-shadow: 0px 1px 10px 3px rgba(189, 177, 177, 0.95);
+  background-color: rgb(255, 255, 255);
+  transform: scale(1.02);
 }
-.presentation{
+.presentation {
   align-items: baseline;
   display: flex;
   padding: 0 16px;
   margin-top: 20px;
 }
 
-.presentation h4{
+.presentation h4 {
   margin-left: 7px;
 }
 
-.img-id{
+.img-id {
   width: 25px;
   height: 25px;
 }
 .divos {
-  margin : 10px auto;
-  text-transform:none;
+  margin: 10px auto;
+  text-transform: none;
 }
 
-#btn_icon{
+#btn_icon {
   height: 20px;
   width: 20px;
   margin-right: 10px;
 }
 
-.link-a:hover{
+.link-a:hover {
   background: none;
-  color:#616161;
+  color: #616161;
 }
 
-.link-a{
+.link-a {
   color: white;
 }
 
-.btn_createpost{
+.btn_createpost {
   background-color: #616161;
-  text-transform:none;
+  text-transform: none;
   box-shadow: 3px 5px 5px rgba(189, 177, 177, 0.95);
   border-radius: 0;
 }
 
-.btn_createpost:hover{
+.btn_createpost:hover {
   background-color: rgb(245, 239, 239);
-  text-transform:none;
+  text-transform: none;
 }
 
-#titre_disposition{
+#titre_disposition {
   padding: 5px 16px;
   font-weight: bold;
   margin: 0 auto;
 }
 
-#dateCreation{
+#dateCreation {
   font-style: italic;
   font-size: smaller;
 }
 
-#btn-logout{
+#btn-logout {
   display: block;
-  position:absolute;
+  position: absolute;
   right: 0;
 }
 
-.titre_deconnecte{
+.titre_deconnecte {
   margin: 30px auto;
 }
 
-#img-attachment{
+#img-attachment {
   width: 70%;
   height: auto;
   margin: 10px auto;
   justify-content: center;
-  display :flex;
+  display: flex;
   box-shadow: 0px 1px 10px 3px rgba(155, 146, 146, 0.95);
-
 }
 
-#deletePost{
+#deletePost {
   text-transform: initial;
   color: rgba(213, 189, 189, 0.95);
-  font-style:normal;
+  font-style: normal;
   position: absolute;
   right: 0;
 }
@@ -214,20 +236,18 @@ export default {
     padding: 0rem;
   }
 
-  .card-remix{
-    padding-left: 0; padding-right: 0;
+  .card-remix {
+    padding-left: 0;
+    padding-right: 0;
   }
-  
-  #titre_disposition{
+
+  #titre_disposition {
     padding: 10px 16px;
   }
 
-  .titre_deconnecte{
-  padding: 0px 10px;
-  font-size: 18px;
+  .titre_deconnecte {
+    padding: 0px 10px;
+    font-size: 18px;
+  }
 }
-
-
-}
-
 </style>
